c6e7f44a3c80835a6449a01144b4f4f8706201b6
belt71tula <60920893+belt71tula@users.noreply.github.com>
2022-07-25 08:48:46 +0300

    1st

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import './App.css'
 
+
+const cardImages = [
+  { 'src': '/public/img/helmet-1.png'},
+  { 'src': '/public/img/potion-1.png'},
+  { 'src': '/public/img/ring-1.png'},
+  { 'src': '/public/img/scroll-1.png'},
+  { 'src': '/public/img/shield-1.png'},
+  { 'src': '/public/img/sword-1.png'}
+]
+
 function App() {
   return (
     <div className="App">
