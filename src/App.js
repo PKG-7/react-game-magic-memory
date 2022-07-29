4438f120e28974a445d279b9e7b506c355c33473
PKG-7 <pavel.k.group@gmail.com>
2022-07-29 07:05:47 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import SingleCard from './components/SingleCard'
 
 
 
 const cardImages = [
-  { 'src': '/img/helmet-1.png'},
-  { 'src': '/img/potion-1.png'},
-  { 'src': '/img/ring-1.png'},
-  { 'src': '/img/scroll-1.png'},
-  { 'src': '/img/shield-1.png'},
-  { 'src': '/img/sword-1.png'}
+  { 'src': '/img/potion-1.png', matched: false},
+  { 'src': '/img/helmet-1.png', matched: false},
+  { 'src': '/img/ring-1.png', matched: false},
+  { 'src': '/img/scroll-1.png', matched: false},
+  { 'src': '/img/shield-1.png', matched: false},
+  { 'src': '/img/sword-1.png', matched: false}
 ]
 
 function App() {
@@ -34,6 +34,39 @@ function App() {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
   }
 
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo){
+
+      if(choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        setTimeout(() => {
+          resetTurn()
+        }, 1000);
+        
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  console.log(cards);
+
+  // reset choices & increase run
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+  }
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
@@ -46,6 +79,7 @@ function App() {
            key={card.id} 
            card={card}
            handleChoice={handleChoice}
+           flipped={card === choiceOne || card === choiceTwo || card.matched }
            />
 
         ))}
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,6 +1,6 @@
 import './SingleCard.css'
 
-export default function SingleCard({card, handleChoice}) {
+export default function SingleCard({card, handleChoice, flipped}) {
 
   const handleClick = () => {
     handleChoice(card)
@@ -8,7 +8,7 @@ export default function SingleCard({card, handleChoice}) {
 
   return (
         <div className='card'>
-            <div>
+            <div className={flipped ? "flipped" : ""}>
               <img className='front' src={card.src} alt='card front' />
 
               <img
