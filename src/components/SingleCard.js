11c342d71c081cc160293be57c550f9a2bb3fbd8
PKG-7 <pavel.k.group@gmail.com>
2023-06-15 00:39:34 +0300

    replaced bg image

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -14,7 +14,7 @@ export default function SingleCard({ card, handleChoice, flipped, disabled }) {
 
                 <img
                     className='back'
-                    src='/img/cover.png'
+                    src='/img/cover.jpg'
                     onClick={handleClick}
                     alt='card back'
                 />
